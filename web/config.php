2eb6dfba10413b52301c318323de9385d9824326
morty000 <41410719+morty000@users.noreply.github.com>
2020-10-10 23:02:33 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');//这里改成您的api key
+define('APIKEY', 'AIzaSyBfOROA77kJyIp2G_verpG7tsiFieuRwNo');//这里改成您的api key
 define('GJ_CODE', 'NZ');//国家代码
-define('SITE_NAME', 'Share2018');//网站名字
-define('TITLENAME', '共享網飛群&遊管');//网站名字
+define('SITE_NAME', 'morty233');//网站名字
+define('TITLENAME', 'morty233');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'wingwanwin@gmail.com');//邮箱
+define('EMAIL', 'moyiguo000@gmail.com');//邮箱
 ?>
